4dca1175503b21277de6b116bfe5c391b81fbc0c
SANJAY K SAI <shachsan@hotmail.com>
2018-11-03 22:04:06 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,8 +13,9 @@ class Scraper
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
-      course.description = post.css("p").text 
+      course.description = post.css("p").text
     end
+    
 
   end
 
