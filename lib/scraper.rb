44146e2c214406160631751abd3d2789b1ae9102
SANJAY K SAI <shachsan@hotmail.com>
2018-11-03 21:29:56 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
-    open()
+    open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
   end
 
     def print_courses
